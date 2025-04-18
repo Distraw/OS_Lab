9a0c16b07454238371d93e2c052b5c970c2d3898
Distraw <dmitroisuk11@gmail.com>
2025-04-18 23:04:57 +0300

    ready

--- Lab_4/Task_6/main.py
+++ Lab_4/Task_6/main.py
@@ -1,63 +1,48 @@
 import os
-import signal
 import time
+import signal
+import sys
 
-# Глобальні лічильники для сигналів
-usr1_count = 0
-rtmin_count = 0
+sigrtmin_occurence = 0
+sigusr1_occurence = 0
 
+def sigrtmin_handler(signum, frame):
+    global sigrtmin_occurence
+    sigrtmin_occurence += 1
 
-def handle_usr1(signum, frame):
-    global usr1_count
-    usr1_count += 1
+def sigusr1_handler(signum, frame):
+    global sigusr1_occurence
+    sigusr1_occurence += 1
 
-
-def handle_rtmin(signum, frame):
-    global rtmin_count
-    rtmin_count += 1
-
-
-def child_process():
-    global usr1_count, rtmin_count
-
-    #signal.pthread_sigmask(signal.SIG_BLOCK, {signal.SIGUSR1, signal.SIGRTMIN})
-
-    print(f"Дочірній процес (PID: {os.getpid()}) готовий до отримання сигналів.")
-
-    os.kill(os.getppid(), signal.SIGUSR2)
-
-    time.sleep(5)
-
-    #signal.pthread_sigmask(signal.SIG_UNBLOCK, {signal.SIGUSR1, signal.SIGRTMIN})
-
-    print(f"Дочірній процес (PID: {os.getpid()}) обробив сигнали:")
-    print(f"SIGUSR1: {usr1_count} сигналів")
-    print(f"SIGRTMIN: {rtmin_count} сигналів")
-
-
-def parent_process(child_pid):
-    print(f"Батьківський процес (PID: {os.getpid()}) чекає на готовність дитини.")
-    signal.pause()
-
+def parent_process(signum, frame):
+    print("Parent: sending signals...")
     for _ in range(10):
-        os.kill(child_pid, signal.SIGUSR1)
+        os.kill(pid, signal.SIGRTMIN)
     for _ in range(10):
-        os.kill(child_pid, signal.SIGRTMIN)
+        os.kill(pid, signal.SIGUSR1)
 
-    time.sleep(5)
-    os.kill(child_pid, signal.SIGTERM)
+    print("Parent: signals were sent")
+    time.sleep(3)
 
-    print("Батьківський процес завершив свою роботу.")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     pid = os.fork()
-
     if pid == 0:
-        signal.signal(signal.SIGUSR1, handle_usr1)
-        signal.signal(signal.SIGRTMIN, handle_rtmin)
-        signal.signal(signal.SIGTERM, lambda signum, frame: exit(0))
+        signal.signal(signal.SIGRTMIN, sigrtmin_handler)
+        signal.signal(signal.SIGUSR1, sigusr1_handler)
 
-        child_process()
+        signal.pthread_sigmask(signal.SIG_SETMASK, {signal.SIGRTMIN, signal.SIGUSR1})
 
+        print("Child: ready to receive signals")
+        os.kill(os.getppid(), signal.SIGUSR2)
+
+        time.sleep(2)
+
+        signal.pthread_sigmask(signal.SIG_UNBLOCK, {signal.SIGRTMIN, signal.SIGUSR1})
+        print("Child: signals received")
+
+        print("SIGRTMIN: ", sigrtmin_occurence)
+        print("SIGUSR1: ", sigusr1_occurence)
+        sys.exit(0)
     else:
-        parent_process(pid)
+        signal.signal(signal.SIGUSR2, parent_process)
+        signal.pause()
